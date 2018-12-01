7bff7e2ca62cf538966ca848af3454c1022b2b49
KingJeremy <kingjeremy2211@hotmail.com>
2018-12-01 16:40:46 -0600

    try

--- client/src/index.js
+++ client/src/index.js
@@ -11,19 +11,19 @@ render(
 )
 
 // Check for browser support of service worker
-if ('serviceWorker' in navigator) {
- navigator.serviceWorker.register('./registerServiceWorker')
- .then(function(registration) {
-   // Successful registration
-   console.log('Hooray. Registration successful, scope is:', registration.scope);
+// if ('serviceWorker' in navigator) {
+//  navigator.serviceWorker.register('./registerServiceWorker')
+//  .then(function(registration) {
+//    // Successful registration
+//    console.log('Hooray. Registration successful, scope is:', registration.scope);
 
- }).catch(function(err) {
-   // Failed registration, service worker won’t be installed
-   console.log('Whoops. Service worker registration failed, error:', err);
+//  }).catch(function(err) {
+//    // Failed registration, service worker won’t be installed
+//    console.log('Whoops. Service worker registration failed, error:', err);
     
 
- });
-}
+//  });
+// }
 
 document.addEventListener('scroll', function (event) {
     if (document.body.scrollHeight ===
