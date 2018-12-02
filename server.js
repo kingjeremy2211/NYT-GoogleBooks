00caf92f72ab6ccc1b1bd90e7ba7af8f4034bf05
KingJeremy <kingjeremy2211@hotmail.com>
2018-12-01 22:00:11 -0600

    super close

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const options = {
       allow : {
           origin: '*',
