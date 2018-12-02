c9a27985f58b6c4efe2f728cf8b9d064ceb864e9
KingJeremy <kingjeremy2211@hotmail.com>
2018-12-01 20:00:30 -0600

    369

---
+++ client/build/precache-manifest.6a9e6ac86e624c9f7f3e600cab9897e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c73c27c9716db1b333e",
+    "url": "/static/js/main.7c73c27c.chunk.js"
+  },
+  {
+    "revision": "66002c78132816be6cdb",
+    "url": "/static/js/1.66002c78.chunk.js"
+  },
+  {
+    "revision": "7c73c27c9716db1b333e",
+    "url": "/static/css/main.cc524e34.chunk.css"
+  },
+  {
+    "revision": "34022a576d448c9c8cac3cb77c97d113",
+    "url": "/index.html"
+  }
+];
